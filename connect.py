4d49a482cdcf2e226e6533ebf4b69fc3d1950dda
Deminform <deminform@gmail.com>
2024-10-04 15:59:58 +0200

    0.1.3

--- connect.py
+++ connect.py
@@ -11,5 +11,4 @@ db_name = config.get('DB', 'db_name')
 domain = config.get('DB', 'domain')
 
 # connect to cluster on AtlasDB with connection string
-
 connect(host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""", ssl=True)
